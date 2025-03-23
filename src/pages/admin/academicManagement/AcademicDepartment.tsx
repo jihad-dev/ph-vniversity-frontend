b327723bb04a9b62dd4d3d03ea107416a3b091df
jihad <mdjihadislam166@gmail.com>
2025-03-23 19:07:45 +0600

    redux api setup

--- src/pages/admin/academicManagement/AcademicDepartment.tsx
+++ src/pages/admin/academicManagement/AcademicDepartment.tsx
@@ -1,12 +1,68 @@
 
 
+import { Table, TableColumnsType, TableProps } from "antd";
+import { useGetAcademicDepartmentsQuery } from "../../../redux/features/admin/academicManagement.api";
+
+
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
 const AcademicDepartment = () => {
+  const {
+    data: departmentData,
+    isFetching,
+    isLoading,
+  } = useGetAcademicDepartmentsQuery(undefined);
+
+  const tableData = departmentData?.data?.map((department: any) => ({
+    name: department?.name,
+    key: department?._id,
+  }));
+
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "Department Name",
+      dataIndex: "name",
+    },
+  ];
+
+  const onChange: TableProps<DataType>["onChange"] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
+  if (isLoading) {
     return (
-        <div>
-            
-            AcademicDepartment
-        </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          fontSize: "20px",
+          
+
+        }}
+      >
+        Loading...
+      </div>
     );
+  }
+  return (
+    <Table<DataType>
+      columns={columns}
+      loading={isFetching}
+      dataSource={tableData}
+      onChange={onChange}
+    />
+  );
 };
 
-export default AcademicDepartment;
+export default AcademicDepartment        ;
--- src/pages/admin/academicManagement/CreateAcademicDepartment.tsx
+++ src/pages/admin/academicManagement/CreateAcademicDepartment.tsx
@@ -1,11 +1,66 @@
+import { FieldValues } from "react-hook-form";
+import PhForm from "../../../components/form/PhForm";
+import PhInput from "../../../components/form/PhInput";
+import { Button, Col, Flex } from "antd";
+import PhSelect from "../../../components/form/PhSelect";
+import {
+  useCreateAcademicDepartmentMutation,
+  useGetAcademicFacultyQuery,
+} from "../../../redux/features/admin/academicManagement.api";
+import { toast } from "sonner";
+
 
 
 const CreateAcademicDepartment = () => {
-    return (
-        <div>
-           CreateAcademicDepartment
-        </div>
-    );
+
+  const [createAcademicDepartment] = useCreateAcademicDepartmentMutation();
+  const departmentOptions = useGetAcademicFacultyQuery(undefined);
+  const onsubmit = async (data: FieldValues) => {
+    const departmentData = {
+      name: data.name,
+      academicFaculty: data.academicFaculty,
+    };
+    const toastId = toast.loading("Creating academic Department");
+
+    try {
+      const res = await createAcademicDepartment(departmentData);
+      console.log(res);
+      if ("error" in res) {
+        toast.error((res.error as any)?.data?.message, { id: toastId });
+      } else {
+        toast.success("Academic Department created successfully", {
+          id: toastId,
+        });
+      }
+    } catch (error) {
+      toast.error("Something went wrong", { id: toastId });
+    }
+  };
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PhForm onsubmit={onsubmit}>
+          <PhInput
+            
+            name="name"
+            label="Department name"
+            type="text"
+          />
+          <PhSelect
+            name="academicFaculty"
+            label="Faculty"
+            options={
+              departmentOptions.data?.data?.map((faculty: any) => ({
+                value: faculty._id,
+                label: faculty.name,
+              })) || []
+            }
+          />
+          <Button htmlType="submit">submit</Button>
+        </PhForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default CreateAcademicDepartment;
